40da3b0aea70b44103ad43d842262f36a8b0f78d
Gitjeshub <38493098+Gitjeshub@users.noreply.github.com>
2021-08-26 14:50:39 +0200

    Set up database seeding for users

--- app/src/main/java/com/example/myapplication/application/JobHuntApplication.kt
+++ app/src/main/java/com/example/myapplication/application/JobHuntApplication.kt
@@ -3,9 +3,17 @@ package com.example.myapplication.application
 import android.app.Application
 import com.example.myapplication.models.database.JobHuntRoomDatabase
 import com.example.myapplication.models.database.user.UserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class JobHuntApplication : Application() {
-    private val database by lazy { JobHuntRoomDatabase.getDatabase((this@JobHuntApplication)) }
+    private val database by lazy {
+        runBlocking {
+            JobHuntRoomDatabase.getDatabase((this@JobHuntApplication))
+        }
+    }
 
     val userRepository by lazy { UserRepository(database.userDao()) }
 }
--- app/src/main/java/com/example/myapplication/models/database/JobHuntRoomDatabase.kt
+++ app/src/main/java/com/example/myapplication/models/database/JobHuntRoomDatabase.kt
@@ -9,8 +9,13 @@ import com.example.myapplication.models.database.interview.InterviewDao
 import com.example.myapplication.models.database.jobOffer.JobOfferDao
 import com.example.myapplication.models.database.user.UserDao
 import com.example.myapplication.models.entities.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [User::class, JobOffer::class, Interview::class], version = 2)
+
+@Database(entities = [User::class, JobOffer::class, Interview::class], version = 1)
 @TypeConverters(StatusToIntConverter::class, UserTypeIntConverter::class, DateConverter::class)
 abstract class JobHuntRoomDatabase : RoomDatabase() {
 
@@ -21,20 +26,142 @@ abstract class JobHuntRoomDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: JobHuntRoomDatabase? = null
+        private val mutex = Mutex()
 
-        fun getDatabase(context: Context): JobHuntRoomDatabase {
+        suspend fun getDatabase(context: Context): JobHuntRoomDatabase {
             //IF THE INSTANCE IS NOT NULL, RETURN IT
             //IF IT IS NULL, CREATE THE DATABASE
-            return INSTANCE ?: synchronized(this) {
+            return INSTANCE ?: mutex.withLock {
+                var dbCallback: Callback = object : Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        MainScope().launch(Dispatchers.IO) {
+                            seedData()
+                        }
+                    }
+                }
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     JobHuntRoomDatabase::class.java,
                     "jobhunt_database"
-                ).fallbackToDestructiveMigration()
+                )
+                    .addCallback(dbCallback)
+                    .fallbackToDestructiveMigration()
                     .build()
                 INSTANCE = instance
+
                 instance
             }
+        }
+
+        private suspend fun seedData() {
+            //region seeding.users
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "first",
+                    "user",
+                    "",
+                    "Lommel",
+                    null,
+                    0,
+                    "",
+                    "first@user.be",
+                    "test"
+                )
+            )
+
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "second",
+                    "user",
+                    "",
+                    "Pelt",
+                    null,
+                    0,
+                    "",
+                    "second@user.be",
+                    "test"
+                )
+            )
+
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "third",
+                    "user",
+                    "",
+                    "Mol",
+                    null,
+                    0,
+                    "",
+                    "third@user.be",
+                    "test"
+                )
+            )
+
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "fourth",
+                    "user",
+                    "",
+                    "Geel",
+                    null,
+                    0,
+                    "",
+                    "fourth@user.be",
+                    "test"
+                )
+            )
+
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "",
+                    "",
+                    "Pepsi",
+                    "Hasselt",
+                    null,
+                    1,
+                    "",
+                    "Pepsi@Hasselt.be",
+                    "pepsi"
+                )
+            )
+
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "",
+                    "",
+                    "Cola",
+                    "Hasselt",
+                    null,
+                    1,
+                    "",
+                    "Cola@Hasselt.be",
+                    "cola"
+                )
+            )
+
+            INSTANCE!!.userDao().insertUser(
+                User(
+                    0,
+                    "",
+                    "",
+                    "IKEA",
+                    "Hasselt",
+                    null,
+                    1,
+                    "",
+                    "Ikea@Hasselt.be",
+                    "ikea"
+                )
+            )
+
+            //endregion seeding.users
         }
     }
 }
